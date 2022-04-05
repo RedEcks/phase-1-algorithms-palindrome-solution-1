a345a7e27153ac9eeb56ba21d3e08167d4f8d42d
Xavier Dunkley <xavierxv18@gmail.com>
2022-04-04 22:35:55 -0400

    commit tree bc07bae4a8713e5204f1108d6908bff36dcc9584

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+function stringReverse(word){
+  //'abc'=>'cba'
+  let wordStringArray = word.split("")
+  let reverseStringArray = wordStringArray.reverse()
+  let reverseWord = reverseStringArray.join("")
+
+  return reverseWord
+  //another way is as follows:
+  //return word.split("").reverse().join("")
+}
+
+
+
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = stringReverse(word)
+  if(word ===reversedWord){
+    return true
+  }else{
+    return false
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  if string is equal to reverse string then return true
+  else return false 
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
